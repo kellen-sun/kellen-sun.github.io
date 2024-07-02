9a182d3372f509aca7a788f0e4b973f3840ec856
kellen-sun <ksun5@ocdsb.ca>
2024-07-02 15:23:13 -0400

    updates to background image

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 function PageNotFound() {
   return (
     <div className='pageNotFound' style={{ backgroundImage: `url(${BannerImage})` }}>
-      <h1>404 Error!</h1>
+      <h1>Oh No! 404 Error!</h1>
       <p>This webpage wasn't found. Check your URL. If you're sure you're at the correct URL, please contact me.</p>
       <h2>Or you can just go back <Link to="/">home</Link>.</h2>
     </div>
--- src/pages/Writings.js
+++ src/pages/Writings.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import '../styles/About.css';
-import Mando from '../assets/mando.PNG';
+import BannerImage from '../assets/orange.jpg';
 
 function Writings() {
 
   return (
     <div className = "about" style={{ backgroundColor: '#f0d0d0' }}>
-      <div className='aboutTop' style={{ backgroundImage: `url(${Mando})` }}></div>
+      <div className='aboutTop' style={{ backgroundImage: `url(${BannerImage})` }}></div>
       <div className='aboutBottom'>
         <h1>Some writings...</h1>
         <p>I think, therefore I am. But I don't really think until I write thoughts out. Here are lists of thoughts I've had (whatever that means) over the years that I've collected. Some are academic notes and some are notes from books I've read. I hope to expand upon these and form a sort of blog.</p>
